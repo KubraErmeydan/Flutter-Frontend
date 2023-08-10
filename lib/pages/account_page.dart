cee7d89bbf36f6e39aad88074e53500903f2c360
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-08-10 11:19:30 +0300

    extracts added

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -43,10 +43,47 @@ class _AccountPageState extends State<AccountPage> {
                           Text('Account',style: TextStyle(fontSize: 18),),
                           Row(
                             children: [
-                              Image.asset('asssets/notification.png'),
-                              TextButton(onPressed: (){}, child: Text('Notification')),
+                              Image.asset('assets/notification.png',height:25,),
+                              TextButton(onPressed: (){}, child: Text('Notification',style: TextStyle(color: Colors.black,fontSize: 18, fontWeight: FontWeight.normal))),
                             ],
-                          )
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/calender.png',height:25,),
+                              TextButton(onPressed: (){}, child: Text('My Bookings',style: TextStyle(fontWeight:FontWeight.normal ,color: Colors.black,fontSize: 18))),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/tick.png',height: 25,),
+                              TextButton(onPressed: (){}, child: Text('My Plan',style: TextStyle(fontWeight:FontWeight.normal,color: Colors.black,fontSize: 18))),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/address.png',height: 25,),
+                              TextButton(onPressed: (){}, child: Text('Addresses',style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.normal))),
+                            ],
+                          ),
+                          Text('Share',style: TextStyle(fontSize: 18),),
+                          Row(
+                            children: [
+                              Image.asset('assets/facebook.png',height: 25,),
+                              TextButton(onPressed: (){}, child: Text('Facebook',style: TextStyle(fontWeight:FontWeight.normal,color: Colors.black,fontSize: 18))),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/twitter.png',height: 25,),
+                              TextButton(onPressed: (){}, child: Text('Twitter',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.black,fontSize: 18))),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/gmail.png',height: 25,),
+                              TextButton(onPressed: (){}, child: Text('Gmail',style: TextStyle(fontWeight: FontWeight.normal,color: Colors.black,fontSize: 18))),
+                            ],
+                          ),
                         ],
                       ),
                     ),
